d3263e49fdc8cc87bcdc608b32f43ff1a6f7a8ea
jorritvanderheide <35707261+jorritvanderheide@users.noreply.github.com>
2025-01-02 10:53:24 +0100

    fix: idle power draw issue

--- framework/13-inch/12th-gen-intel/default.nix
+++ framework/13-inch/12th-gen-intel/default.nix
@@ -12,7 +12,6 @@
     {
       hardware.intelgpu.loadInInitrd = lib.versionOlder config.boot.kernelPackages.kernel.version "6.2";
       # same as 13th gen framework 13-inch
-      hardware.framework.laptop13.audioEnhancement.enable = true;
       hardware.framework.laptop13.audioEnhancement.rawDeviceName = lib.mkDefault "alsa_output.pci-0000_00_1f.3.analog-stereo";
     }
     # https://community.frame.work/t/tracking-hard-freezing-on-fedora-36-with-the-new-12th-gen-system/20675/391
@@ -38,7 +37,7 @@
         # https://kvark.github.io/linux/framework/2021/10/17/framework-nixos.html
         # Update 04/2024: Combined with acpi_osi from framework-intel it increases the idle power-usage in my test (SebTM)
         # (see: https://github.com/NixOS/nixos-hardware/pull/903#issuecomment-2068146658)
-        "mem_sleep_default=deep"
+        # "mem_sleep_default=deep"
       ];
 
       # Further tweak to ensure the brightness and airplane mode keys work
